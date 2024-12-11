9fe1fb3e47f32aac2554726bad79a7e0f922f78c
QHU008 <QHU008@e.ntu.edu.sg>
2024-12-11 03:07:56 +0000

    3.4

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+
 if __name__=="__main__":
     app.run()
